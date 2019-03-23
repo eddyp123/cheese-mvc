a72fdc83691949545b2761b193c0c65fac5b0e1e
Edward Prenzler <edpren12@gmail.com>
2019-03-23 18:01:02 -0500

    added add and remove methods

--- cheese-mvc/cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ cheese-mvc/cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
-    //cheeses ArrayList is only in memory when the program is running. Not a substitute for a database.
-    static ArrayList<String> cheeses = new ArrayList<>();
+    //construct arraylist and pass in the Cheese object
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     // request path for all methods in this class: /cheese
     @RequestMapping(value = "")
@@ -33,11 +35,38 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseFrom(@RequestParam String cheeseName){ //@RequestParam expects to be passed a parameter named cheeseName
-        cheeses.add(cheeseName);                 //Spring looks for the request parameter in the post request form in /add
+    public String processAddCheeseFrom(@RequestParam String cheeseName, @RequestParam String cheeseDesc){ //@RequestParam expects to be passed a parameter named cheeseName
+        //cheeses.add(cheeseName);//Spring looks for the request parameter in the post request form in /add
+
+        Cheese cheese = new Cheese(cheeseName, cheeseDesc);
+        cheeses.add(cheese);
+
         //redirect to /cheese
         return "redirect:";
 
     }
+// request path cheese/remove
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST) //checkboxes store values in arraylists, so we pass on in here
+    public String processRemoveCheeseForm(@RequestParam ArrayList<Integer> cheeseIds){
+        for (Integer cheeseId : cheeseIds ) {
+            for (Cheese cheese : cheeses){
+                if (cheese.getId() == cheeseId ) {
+                    cheeses.remove(cheese);
+                    break;
+                }
+            }
+        }
+
+        return "redirect:";
+    }
+
+
 }
 
